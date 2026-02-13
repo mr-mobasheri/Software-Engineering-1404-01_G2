52ba9c335f339468606c5b8e0a059885b0062342
mr-mobasheri <mr.mobasheri@aut.ac.ir>
2026-02-13 21:46:51 +0330

    Map Service

--- team9/urls.py
+++ team9/urls.py
@@ -2,8 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.base),
-    path("ping/", views.ping),
-    path("map/", views.map_view),
-    path("api/mock-facilities/", views.mock_facilities_api),
+    path("", views.base, name='home'),
+    path("ping/", views.ping, name='ping'),
+    path("map/", views.map_view, name='map_view'),
 ]
